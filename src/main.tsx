957aa77cc588e4afb129a4170035129ddf486caa
Rovin <rovinpokharel123@gmail.com>
2025-03-15 17:52:21 +0545

    React Router Setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter as Router } from 'react-router-dom'
+import React from 'react'
+import AppRoutes from './AppRoutes'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <React.StrictMode>
+    <Router>
+      <AppRoutes />
+    </Router>
+  </React.StrictMode>,
 )
