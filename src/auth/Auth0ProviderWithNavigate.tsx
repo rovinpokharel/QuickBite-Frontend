bfe98b2aeaf250948fe5ea52f39a09776e05fc82
Rovin <rovinpokharel123@gmail.com>
2025-03-16 23:46:41 +0545

    Validate access token

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,30 +1,29 @@
-import { useCreateMyUser } from "@/api/MyUserApi";
-import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
-// import { useNavigate } from "react-router-dom";
+// import { useCreateMyUser } from "@/api/MyUserApi";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
-  const {createUser} = useCreateMyUser();
-//   const navigate = useNavigate();
-
+  const navigate = useNavigate();
+  // const {getAccessTokenSilently} = useAuth0();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-//   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-//   if (!domain || !clientId || !redirectUri || !audience) {
-if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("unable to initialise auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) => {
+  const onRedirectCallback = () => {
     // navigate(appState?.returnTo || "/auth-callback");
-    if(user?.sub && user?.email){
-      createUser({auth0Id: user.sub, email: user.email})
-    }
+    // const token = await getAccessTokenSilently();
+    // console.log("token", token);
+    navigate("/auth-callback")
+
   };
 
   return (
@@ -33,11 +32,11 @@ if (!domain || !clientId || !redirectUri) {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
-//         audience,
+        audience,
       }}
       onRedirectCallback={onRedirectCallback}
     >
-       {children}
+      {children}
     </Auth0Provider>
   );
 };
