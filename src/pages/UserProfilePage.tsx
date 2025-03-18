0e59354f72b25fdfc7dd6c668ac0c0bc4d23a02c
Rovin <rovinpokharel123@gmail.com>
2025-03-18 22:42:27 +0545

    Toast Notifications

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,13 +1,27 @@
 // import React from 'react'
 
+import { useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 
 export default function UserProfilePage() {
+    // const { currentUser, isLoading: isGetLoading } = useGetMyUser();
+//   const { updateUser, isLoading: isUpdateLoading } = useUpdateMyUser();
+  const { updateUser, isLoading } = useUpdateMyUser();
+
+//   if (isGetLoading) {
+//     return <span>Loading...</span>;
+//   }
+
+//   if (!currentUser) {
+//     return <span>Unable to load user profile</span>;
+//   }
+
   return (
     <UserProfileForm
     //   currentUser={currentUser}
-    //   onSave={updateUser}
+      onSave={updateUser}
     //   isLoading={isUpdateLoading}
+      isLoading={isLoading}
     />
   )
 }
