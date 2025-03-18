90c21f4ebf006adb714d900086a1189997abe0df
Rovin <rovinpokharel123@gmail.com>
2025-03-18 22:16:47 +0545

    User profile form

---
+++ src/pages/UserProfilePage.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
+
+export default function UserProfilePage() {
+  return (
+    <UserProfileForm
+    //   currentUser={currentUser}
+    //   onSave={updateUser}
+    //   isLoading={isUpdateLoading}
+    />
+  )
+}
