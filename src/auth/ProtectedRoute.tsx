4c528134afd44644bc51ea0a13cfeea46fecb6d7
Rovin <rovinpokharel123@gmail.com>
2025-03-20 08:14:02 +0545

    Protect user profile router

---
+++ src/auth/ProtectedRoute.tsx
@@ -0,0 +1,20 @@
+import { useAuth0 } from '@auth0/auth0-react';
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom';
+
+export default function ProtectedRoute() {
+    // const { isAuthenticated, isLoading } = useAuth0();
+    const { isAuthenticated } = useAuth0();
+
+    // if (isLoading) {
+    //     return null;
+    // }
+
+    // if (isAuthenticated) {
+    //     return <Outlet />;
+    // }
+    // return (
+    //     <div>ProtectedRoute</div>
+    // )
+    return isAuthenticated ? (<Outlet />) : (<Navigate to="/" replace/>)
+}
