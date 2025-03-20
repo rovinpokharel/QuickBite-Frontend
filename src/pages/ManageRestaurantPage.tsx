1197c1e91de45474acd9d7ff9551b352d2535dce
Rovin <rovinpokharel123@gmail.com>
2025-03-21 01:31:44 +0545

    Create restaurant API request

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,5 +1,7 @@
+import { useCreateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 export default function ManageRestaurantPage() {
-  return <ManageRestaurantForm />;
+  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  return <ManageRestaurantForm onSave={createRestaurant} isLoading={isLoading} />;
 }
