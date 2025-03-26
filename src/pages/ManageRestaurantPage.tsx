6ac60dd9b272a3c9a6a8840326ff903b70e3d3fb
Rovin <rovinpokharel123@gmail.com>
2025-03-26 15:26:24 +0545

    Update my restaurant request and manage restaurant deploy

--- src/pages/ManageRestaurantPage.tsx
+++ src/pages/ManageRestaurantPage.tsx
@@ -1,11 +1,18 @@
-import { useCreateMyRestaurant, useGetMyRestaurant } from "@/api/MyRestaurantApi";
+import { useCreateMyRestaurant, useGetMyRestaurant, useUpdateMyRestaurant } from "@/api/MyRestaurantApi";
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 
 export default function ManageRestaurantPage() {
-  const { createRestaurant, isLoading } = useCreateMyRestaurant();
+  const { createRestaurant, isLoading: isCreateLoading } = useCreateMyRestaurant();
   const { restaurant } = useGetMyRestaurant();
+  const { updateRestaurant, isLoading: isUpdateLoading } = useUpdateMyRestaurant();
+
+  const isEditing = !!restaurant;
 
   return (
-    <ManageRestaurantForm restaurant={restaurant} onSave={createRestaurant} isLoading={isLoading} />
+    <ManageRestaurantForm
+      restaurant={restaurant}
+      onSave={isEditing ? updateRestaurant : createRestaurant}
+      isLoading={isCreateLoading || isUpdateLoading}
+    />
   );
 }
