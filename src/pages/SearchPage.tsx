48c58cc0edbc67c3a4418a055f9ccf91b0d916b9
Rovin <rovinpokharel123@gmail.com>
2025-03-26 19:45:23 +0545

    Search API request

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,7 +1,20 @@
+import { useSearchRestaurants } from "@/api/RestaurantApi";
 import { useParams } from "react-router-dom";
 
 export default function SearchPage() {
     const { city } = useParams();
-    
-    return <span>Search Page for {city}</span>;
+    const { results } = useSearchRestaurants(city);
+
+    return (
+        <span>
+            Search Page for {city}{" "}
+            <span>
+                {results?.data.map((restaurant) => (
+                    <span>
+                        found - {restaurant.restaurantName}, {restaurant.city}
+                    </span>
+                ))}
+            </span>
+        </span>
+    )
 }
