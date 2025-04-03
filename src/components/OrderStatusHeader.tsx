138b69110af929ab2e68af1845703fde76537736
Rovin <rovinpokharel123@gmail.com>
2025-04-04 00:47:43 +0545

    Order status config

--- src/components/OrderStatusHeader.tsx
+++ src/components/OrderStatusHeader.tsx
@@ -1,7 +1,6 @@
 import { Order } from "@/types";
 import { Progress } from "./ui/progress";
-// import { Progress } from "./ui/progress";
-// import { ORDER_STATUS } from "@/config/order-status-config";
+import { ORDER_STATUS } from "@/config/order-status-config";
 
 type Props = {
   order: Order;
@@ -23,22 +22,21 @@ const OrderStatusHeader = ({ order }: Props) => {
     return `${hours}:${paddedMinutes}`;
   };
 
-//   const getOrderStatusInfo = () => {
-//     return (
-//       ORDER_STATUS.find((o) => o.value === order.status) || ORDER_STATUS[0]
-//     );
-//   };
+  const getOrderStatusInfo = () => {
+    return (
+      ORDER_STATUS.find((o) => o.value === order.status) || ORDER_STATUS[0]
+    );
+  };
 
   return (
     <>
       <h1 className="text-4xl font-bold tracking-tighter flex flex-col gap-5 md:flex-row md:justify-between">
-        {/* <span> Order Status: {getOrderStatusInfo().label}</span> */}
-        <span> Order Status: {order.status}</span>
+        <span> Order Status: {getOrderStatusInfo().label}</span>
         <span> Expected by: {getExpectedDelivery()}</span>
       </h1>
       <Progress
         className="animate-pulse"
-        // value={getOrderStatusInfo().progressValue}
+        value={getOrderStatusInfo().progressValue}
       />
     </>
   );
